e71e79756a03e7bda266cbab10cb4b215cabcf58
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-05-23 16:12:41 -0500

    Fixed problem of adminBar state change when nav item is used to changed to another page

--- README.md
+++ README.md
@@ -8,4 +8,11 @@
 - admin/index.tsx
 
 
+### Problems tackled
+>**Adminbar** loses state when one of the nav items in `collapsed state` is clicked to change to another route or page.
+- How do I solve this issue?
+- The behavior desired is as thus, in collapsed state, when nav items are clicked upon to change to another route. The admin bar should remain collapsed after rendering a new page.
+    
+
+
 
--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import Logo from "./Logo";
 import { IconType } from "react-icons";
 import { RiMenuFoldFill, RiMenuUnfoldFill } from "react-icons/ri";
@@ -9,18 +9,63 @@ interface Props {
 }
 
 const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
+  const navRef = useRef<HTMLElement>(null);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const NAV_OPEN_WIDTH = "w-60";
+  const NAV_CLOSE_WIDTH = "w-12";
+  const COLLAPSED_STATE = "collapsed_state";
+
+  function toggleAdminBar(isCollapsed: boolean) {
+    const navClassList = navRef.current?.classList;
+
+    if (!navClassList) return;
+
+    if (isCollapsed) {
+      // Change width of admin bar to min-width and change collapsed state
+      navClassList.remove(NAV_CLOSE_WIDTH);
+      navClassList.add(NAV_OPEN_WIDTH);
+    } else {
+      // not collapsed
+      navClassList.remove(NAV_OPEN_WIDTH);
+      navClassList.add(NAV_CLOSE_WIDTH);
+    }
+  }
+
+  function toggleAdminState() {
+    console.log(isCollapsed);
+    toggleAdminBar(isCollapsed);
+    const newCollapsedState = !isCollapsed;
+    localStorage.setItem(COLLAPSED_STATE, JSON.stringify(newCollapsedState));
+    setIsCollapsed(newCollapsedState);
+  }
+
+  useEffect(() => {
+    // This is used to let admin bar remain collapsed when route changes
+    const collapsedState = localStorage.getItem(COLLAPSED_STATE);
+    if (collapsedState !== null) {
+      const currentCollapsedState = JSON.parse(collapsedState);
+      setIsCollapsed(currentCollapsedState);
+      toggleAdminBar(!currentCollapsedState);
+    } else {
+      setIsCollapsed(false);
+    }
+  }, []);
+
   return (
     <nav
       className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark
     flex flex-col justify-between"
+      ref={navRef}
     >
       {/* Logo */}
       <div>
         <Link href="/admin" className="flex items-center space-x-2 p-3 mb-10">
           <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
-          <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
-            Admin
-          </span>
+          {!isCollapsed && (
+            <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
+              Admin
+            </span>
+          )}
         </Link>
 
         {/* Nav Items */}
@@ -34,7 +79,7 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
         "
             >
               <item.icon size={24} />
-              <span className="ml-2">{item.label}</span>
+              {!isCollapsed && <span className="ml-2">{item.label}</span>}
             </Link>
           ))}
         </div>
@@ -44,9 +89,13 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
       <button
         className="text-highlight-light dark:text-highlight-dark 
       hover:scale-[0.98] transition self-end"
+        onClick={toggleAdminState}
       >
-        <RiMenuFoldFill size={25} />
-        <RiMenuUnfoldFill size={25} />
+        {isCollapsed ? (
+          <RiMenuUnfoldFill size={25} />
+        ) : (
+          <RiMenuFoldFill size={25} />
+        )}
       </button>
     </nav>
   );
